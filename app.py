6fe2dcc48a87065a593ab451bf3c7f44331a00db
NickG35 <ngeorge356@gmail.com>
2024-09-18 17:00:51 -0400

    initializing sql db and creating location model

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABSE_URI'] = 'sqlite:///locations.db'
+
+#initiate database
+db=SQLAlchemy(app)
+
+#create models for db
+class Locations(db.Model):
 
 @app.route('/')
 def index():
