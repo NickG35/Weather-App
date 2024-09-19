2e007ccc8c2ddd8b7e10b07ab2fe2a2e499ffa99
NickG35 <ngeorge356@gmail.com>
2024-09-19 13:30:31 -0400

    added template reload config and location id column

--- app.py
+++ app.py
@@ -4,13 +4,15 @@ from datetime import datetime
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABSE_URI'] = 'sqlite:///locations.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///locations.db'
+app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 #initiate database
 db=SQLAlchemy(app)
 
 #create models for db
 class Locations(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
 
 @app.route('/')
 def index():
