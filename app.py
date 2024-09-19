8f9bb4389bd13f80d896d04b13a2f6d600546e96
NickG35 <ngeorge356@gmail.com>
2024-09-19 16:25:57 -0400

    added more columns for locations model and api key/url

--- app.py
+++ app.py
@@ -10,9 +10,16 @@ app.config['TEMPLATES_AUTO_RELOAD'] = True
 #initiate database
 db=SQLAlchemy(app)
 
+#API endpoint and key
+API_KEY = 'c24adc3699d398ec4a13585f3590d00e'
+API_URL = 'http://api.openweathermap.org/data/2.5/weather'
+
 #create models for db
 class Locations(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    city = db.Column(db.String(100), nullable=False)
+    temperature = db.Column(db.Float, nullable=False)
+    weather_description = db.column(db.String(200), nullable=False)
 
 @app.route('/')
 def index():
